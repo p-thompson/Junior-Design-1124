c6b83e855a892e17892269f1760e88f4df4d91a7
rachel390 <rachel.mills589@gmail.com>
2021-09-06 16:58:40 -0400

    first design for account personalization

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -15,7 +15,73 @@ function App() {
   function handleSubmit(event) {
     event.preventDefault();
   }
-
+  return (
+    
+    <div class="row">
+      <Helmet>
+        <style>{'body { background-color: LightBlue; }'}</style>
+      </Helmet>
+      <h1 class="font-weight-bold py-3 mb-4">
+        <center>Account Settings</center>
+      </h1>
+      <div class="profile">
+        <tr>
+          <div class = "propic">
+          </div>
+        </tr>
+      </div>
+      <div class = "roletable">
+        <table>
+          <h3 class="font-weight-bold py-3 mb-4">
+              <leftCenter>Choose a Role</leftCenter>
+          </h3>
+          <tr>
+            <div class= "roles">
+              <th>Parent</th>
+              <th>Volunteer</th>
+            </div>
+          </tr>
+          <tr>
+            <h4 class="font-weight-bold py-3 mb-4">
+              <leftCenter>Times Needed/Availability:</leftCenter>
+            </h4>
+            <tr>
+              <th class = "tofrom">To:</th>
+              <div class="dropdown">
+                <button class="dropbtn">Choose Time</button>
+                <div class="dropdown-content">
+                  <a href="#">Link 1</a>
+                  <a href="#">Link 2</a>
+                  <a href="#">Link 3</a>
+                </div>
+              </div>
+              <th class = "tofrom">From:</th>
+              <div class="dropdown">
+                <button class="dropbtn">Choose Time</button>
+                <div class="dropdown-content">
+                  <a href="#">Link 1</a>
+                  <a href="#">Link 2</a>
+                  <a href="#">Link 3</a>
+                </div>
+              </div>
+              <h4 class="font-weight-bold py-3 mb-4">
+                <leftCenter>  </leftCenter>
+              </h4>   
+            </tr>
+          </tr>
+          <tr>
+            <div class = "services">  
+            </div>
+            <div class = "save">
+              <button class="savechanges">Save Changes</button>
+            </div>
+          </tr>
+        </table> 
+      </div>
+    </div>    
+  );
+}
+ /*
   return (
     <div className="Login" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
       <Helmet>
@@ -45,7 +111,7 @@ function App() {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-          /> */}
+          /> }
         </Form.Group>
         <Button block size="lg" type="submit" disabled={!validateForm()}>
           Login
@@ -54,5 +120,8 @@ function App() {
     </div>
   );
 }
+*/
+
+
 
 export default App;
