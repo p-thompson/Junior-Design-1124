fe22fb9047c565e8a416c2573462b28d5376dc3d
meghankast <meghankast7@gmail.com>
2021-09-08 14:46:41 -0400

    final login

--- my-app/src/CreateAccount.js
+++ my-app/src/CreateAccount.js
@@ -1,47 +1,41 @@
 // All necessary imports;
-import React, { Component } from 'react';
+import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import {Grid, Paper, Select, Button, FormControl, InputLabel} from "@material-ui/core";
-import './App.css';
+import './CreateAccount.css';
+import { useHistory} from "react-router-dom";
+import {Helmet} from 'react-helmet'
 
-class CreateAccount extends Component {
-// Fields necessary for creating account
-constructor(props){
-  super(props);
-  this.state={
-  username:'',
-  password:'',
-  first_name:'',
-  last_name:'',
-  email:'',
-  cell:'',
-  type: ''
-  }
- }
+function CreateAccount() {
+    const history = useHistory();
+    const goToLogin = () => history.goBack();
 
-render() {
     return (
+        <div className="CreateAccount">
+        <Helmet>
+        <title>ItTakesAVillage</title>
+        </Helmet>
         <MuiThemeProvider>
         <Grid>
-            <Paper elevation={5} style={{padding: 50, height: '60vh', width:280, margin: "100px auto"}}>
+            <Paper elevation={5} style={{padding: 50, height: '70vh', width:280, margin: "100px auto"}}>
+                <Grid align='center'>
                 <h1>Create Account</h1>
+                </Grid>
                 <TextField label='Username' hintText='Enter Username' required fullWidth/>
                 <TextField label='Password' hintText='Enter Password' required fullWidth/>
                 <TextField label='Password' hintText='Confirm Password' required fullWidth/>
                 <TextField label='Full Name' hintText='Full Name' required fullWidth/>
                 <TextField label='Email' hintText='Email' required fullWidth/>
-                <TextField label='Cell' hintText='Cell' required fullWidth/>
+                <TextField label='Cell' hintText='Cell Number' required fullWidth/>
                 <FormControl>
                 <InputLabel>Type</InputLabel>
                 <Select
                     native
                     required
                     displayEmpty
-                    value={this.state.name}
                     fullWidth
-                    onChange={this.handleChange}
                     inputProps={{
                     name: 'name',
                     id: 'name'
@@ -52,15 +46,17 @@ render() {
                     <option value={"admin"}>Admin</option>
                 </Select>
                 </FormControl>
-                <RaisedButton label="Create Account" primary={true} variant="contained" fullWidth style={{margin: '15px 0'}}/>
+                <RaisedButton label="Create Account" primary={true} variant="contained" fullWidth style={{margin: '20px 0'}}/>
                 <Button 
                 disableFocusRipple disableRipple style={{ textTransform: "none" }} 
                 variant="text"
+                fullWidth
+                onClick={goToLogin}
                 color="primary">Back to Login</Button>
             </Paper>
         </Grid>
         </MuiThemeProvider>
+        </div>
     );
-  }
 }
 export default CreateAccount;
