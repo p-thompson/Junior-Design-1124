3fdcd68fefcb9179ff1a1a0fb87ebc1c9153fc8f
Alyssa Behrend <agbehrend99@gmail.com>
2021-10-27 14:38:18 -0400

    json parsing clean up

--- backend/src/main/java/com/village/rest/UserWebService.java
+++ backend/src/main/java/com/village/rest/UserWebService.java
@@ -11,15 +11,12 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Path("/account")
 public class UserWebService {
     public static UserService userService = new UserService();
-    //public static ObjectMapper mapper = new ObjectMapper();
 
     @POST
     @Path("/create")
@@ -34,7 +31,7 @@ public class UserWebService {
 
     @GET
     @Path("/parent/{username}")
-    public User findParentByUsername(@PathParam("username") String username) throws SQLException, JsonProcessingException {
+    public User findParentByUsername(@PathParam("username") String username) throws SQLException {
         return userService.findParentByUsername(username);
         
         
@@ -43,7 +40,7 @@ public class UserWebService {
 
     @GET
     @Path("/volunteer/{username}")
-    public User findVolunteerByUsername(@PathParam("username") String username) throws SQLException, JsonProcessingException {
+    public User findVolunteerByUsername(@PathParam("username") String username) throws SQLException {
         return userService.findVolunteerByUsername(username);
     }
 
