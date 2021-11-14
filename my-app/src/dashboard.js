ed2bb591c748f6fb6dae2ae2f05b24063165e27e
p-thompson <89748939+p-thompson@users.noreply.github.com>
2021-11-14 18:03:46 -0500

    Update dashboard.js

--- my-app/src/dashboard.js
+++ my-app/src/dashboard.js
@@ -1,14 +1,20 @@
 import { Typography } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
-import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
-import React from "react";
-import { useState } from 'react';
 import VillageNavBar from './VillageNavBar';
 import { useHistory } from "react-router-dom";
+import './dashboard.css';
+import React, { useEffect, useState } from 'react'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import UserCard from './UserCard'
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import { width } from '@mui/system';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
         paddingRight: 24, // keep right padding when drawer closed
       },
       appBar: {
-        background: '#6EC77A',
+        background: "#0077c0",
         zIndex: theme.zIndex.drawer + 1,
         transition: theme.transitions.create(['width', 'margin'], {
           easing: theme.transitions.easing.sharp,
@@ -52,19 +58,33 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Dashboard() {
-    const classes = useStyles();
-    const [isOpen, setIsOpen] = useState(false);
 
- 
-  const togglePopup = () => {
-    setIsOpen(!isOpen);
-  }
+  const handleDelete = async (id) => {
+    await fetch('http://localhost:3000/dashboard/' + id, {
+      method: 'DELETE'
+    })
+    const newNotes = notes.filter(note => note.id != id)
+    setNotes(newNotes)
+  }  
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/dashboard')
+      .then(res => res.json())
+      .then(data => setNotes(data))
+  }, [])
+
+  const classes = useStyles();
   const history = useHistory();
   const goToLogin = () => history.push('/');
+  //need to define fname as the names which match the search/ want to match/ have already etc.
+  const fname = 'paige';
+
+  let notes2 = [{fname: "paige", lname: "thompson", note: "hi"}, {fname: "paige", lname: "thompson", note: "hi"}, {fname: "paige", lname: "thompson", note: "hi"}, {fname: "paige", lname: "thompson", note: "hi"}];
 
 
     return (
-        <div className={classes.root}>
+        <div className={classes.root} style = {{overflowY: 'scroll'}}>
             <AppBar position="absolute" color='primary' className={classes.appBar}>
                 <Toolbar clasName={classes.toolbar}>
                     <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}className={classes.title}>It Takes a Village</Typography>
@@ -79,18 +99,39 @@ function Dashboard() {
                 </Toolbar>
             </AppBar>
                 <Grid container className={classes.container}>
-                    <Grid item xs={8} className={classes.grid}>
-                        <Paper className={classes.paper}>
-                            <Typography>These users want to connect with you.</Typography>
+                    <Grid item xs={6} className={classes.grid}>
+                        <Paper className={classes.paper} style= {{width: 375, marginLeft: 98, textAlign: 'center', backgroundColor: '#E1EBEE'}}>
+                            <Typography>Users who want to connect with you:</Typography>
                         </Paper>
                     </Grid>
-                    <Grid item xs={8} className={classes.grid}>
-                        <Paper className={classes.paper}>
-                            <Typography>Users you've connected with.</Typography>
+
+                    <Grid item xs={6} className={classes.grid}>
+                        <Paper className={classes.paper} style= {{width: 375, marginLeft:98, textAlign: 'center', backgroundColor: '#E1EBEE'}}>
+                            <Typography>Users you've connected with:</Typography>
                         </Paper>
                     </Grid>
+
+                    <Paper elevation={5} style={{marginLeft: 80, padding: 6, width:450, backgroundColor: '#E1EBEE', maxHeight: 275, overflow: 'auto'}}>
+                      <GridList cols ={1} style={{justifyContent: 'center'}}>
+                        {notes2.map(note => (
+                          <GridListTile key={note.id} style={{padding: 8, width: 400, height: 'auto'}}>
+                            <UserCard note={note} handleDelete={handleDelete} />
+                          </GridListTile>
+                        ))}
+                      </GridList>
+                    </Paper>
+
+                    <Paper elevation={5} style={{marginLeft: 170, padding: 6, width:450, backgroundColor: '#E1EBEE', maxHeight: 275, overflow: 'auto'}}>
+                      <GridList cols ={1} style={{justifyContent: 'center'}}>
+                        {notes2.map(note => (
+                          <GridListTile key={note.id} style={{padding: 8, width: 400, height: 'auto'}}>
+                            <UserCard note={note} handleDelete={handleDelete} />
+                          </GridListTile>
+                        ))}
+                      </GridList>
+                    </Paper>
                     <Grid item>
-                    <VillageNavBar/>
+                    <VillageNavBar page="home"/>
                     </Grid>
                 </Grid>
         </div>
